a7c0f563312562c4fb9d1b566ce783001c63697d
AbhishekKanade06 <abhishekkanade9102@gmail.com>
2025-07-05 13:47:58 +0530

    Update app.py

--- GAN/app/app.py
+++ GAN/app/app.py
@@ -44,6 +44,6 @@ if generate:
 
 st.markdown("---")
 st.markdown(
-    "<small>Made with ❤ using Streamlit and TensorFlow | [GitHub](https://github.com/)</small>",
+    "<small>Made with ❤ using Streamlit and TensorFlow | [GitHub](https://github.com/AbhishekKanade06/Projects/tree/main/GAN)</small>",
     unsafe_allow_html=True,
 )
